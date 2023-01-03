4b977c045edd492554adc2e0fce0195af7bbe77d
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-01-03 16:26:52 +0530

    Function Folder Added

---
+++ Arrays/Function/arrowFunction.js
@@ -0,0 +1,7 @@
+const add =(a,b) => {
+      let c = a+b;
+      return c;  
+}
+
+let sum = add(1,7);
+console.log(sum);
---
+++ Arrays/Function/basicFunction.js
@@ -0,0 +1,12 @@
+function null_fun(){
+    console.log("function without argument");
+}
+null_fun();
+
+// Function with argument
+function add(a,b){
+    return a+b;
+}
+let sum = add(2,3);
+console.log(sum);
+
