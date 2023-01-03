8c985a52ba01ae23533c0a2337e75e377f48143c
SANJIV GUPTA <90419613+SANJIVGUPTA07@users.noreply.github.com>
2023-01-03 13:48:53 +0530

    Create 4.for_of.js

---
+++ 4.for_of.js
@@ -0,0 +1,8 @@
+const arr = ["a","b","a","b","a"];
+for(const value of arr){
+    console.log(value);
+} 
+console.log("\n another way to print");
+for(const element of arr){
+    console.log(element);
+} 
